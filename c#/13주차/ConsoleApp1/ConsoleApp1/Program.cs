aa3e9af25df633f211a8208d7e43b045539cd6e7
이주훈 <86885227+wngns1101@users.noreply.github.com>
2022-05-23 18:05:37 +0900

    Add files via upload

---
+++ c#/13주차/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TestClass : IBasic
+    {
+        public int TestProperty
+        {
+            get; set;
+        }
+
+        public int TestInstanceMethod()
+        {
+            return 3;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IBasic a = new TestClass(); //부모 - 자식
+            TestClass b = new TestClass();  //자식 - 자식
+            //TestClass c = new IBasic(); //자식 - 부모는 안 됨
+        }
+    }
+}
